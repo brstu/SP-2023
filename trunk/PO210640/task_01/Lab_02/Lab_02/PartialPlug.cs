a26e9cb6a875a035ddd8600dac2738df9a2009a9
Anton Lobarev <loborew@gmail.com>
2023-10-06 20:20:58 +0300

    Add plug for check

---
+++ trunk/PO210640/task_01/Lab_02/Lab_02/PartialPlug.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+
+namespace Lab_02;
+
+public partial class MainWindow : Window
+{
+    private Guid _guid;
+}
+
+public partial class App : Application
+{
+    private Guid _guid;
+}
