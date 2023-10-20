4416252c0b62f536466b8e4e3f9ec915181c0f45
kod1rov.1 <kodirovbehruz54@gmail.com>
2023-10-20 13:51:25 +0300

    Task_04

--- trunk/PO210719/task_04/src/main.py
+++ trunk/PO210719/task_04/src/main.py
@@ -24,17 +24,18 @@ item1 = Item(1, 10, 100, 50, 20)
 item2 = Item(2, 20, 150, 75, 30)
 item3 = Item(3, 15, 120, 60, 40)
 
+const = 'Cost per gram:'
 
 item1.display()
-print("Cost per gram:", item1.calculate_cost_per_gram())
+print(const, item1.calculate_cost_per_gram())
 print("Remaining lifetime:", item1.remaining_lifetime())
 print()
 
 item2.display()
-print("Cost per gram:", item2.calculate_cost_per_gram())
+print(const, item2.calculate_cost_per_gram())
 print("Remaining lifetime:", item2.remaining_lifetime())
 print()
 
 item3.display()
-print("Cost per gram:", item3.calculate_cost_per_gram())
+print(const, item3.calculate_cost_per_gram())
 print("Remaining lifetime:", item3.remaining_lifetime())
