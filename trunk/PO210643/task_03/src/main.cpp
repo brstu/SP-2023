5a37183911b5ddd516dd0071191ac10dca809cee
AlexaBailey <alexaserko@gmail.com>
2023-10-22 20:47:25 +0300

    Add Task_03

--- trunk/PO210643/task_03/src/main.cpp
+++ trunk/PO210643/task_03/src/main.cpp
@@ -7,9 +7,9 @@ int main() {
     string word;
     cout << "Enter word: ";
     cin >> word;
-    auto entropy = new EntropyCalculator();
+    auto entropy = make_unique<EntropyCalculator>();
     double value = entropy->calculateEntropy(word);
-
+    
     cout << "Entropy: " << value << endl;
     return 0;
 }
