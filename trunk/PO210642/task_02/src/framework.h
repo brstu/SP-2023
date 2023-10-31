b896c7ff0e6fe690949112ad18e141f0abc80879
Valeria Pechko <99526775+ValeriaPJul@users.noreply.github.com>
2023-10-31 10:17:05 +0300

    Update trunk/PO210642/task_02/src/framework.h
    Co-authored-by: Yana Sitkovets  <yana.sitkowecz@yandex.ru>

--- trunk/PO210642/task_02/src/framework.h
+++ trunk/PO210642/task_02/src/framework.h
@@ -3,7 +3,7 @@
 //
 
 #pragma once
-
+#include <random>
 #include "targetver.h"
 #define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
 // Файлы заголовков Windows
