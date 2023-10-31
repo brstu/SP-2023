6958a53319d931289e1ab17f0c822a85b89fc424
AlexaBailey <alexaserko@gmail.com>
2023-10-31 21:38:50 +0300

    Add task_05

--- trunk/PO210643/task_05/src/Mutex.cpp
+++ trunk/PO210643/task_05/src/Mutex.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
+static std::mutex mutex;
 
-std::mutex mutex;
-float number = 0.0;
 
-void increment(float value) {
+void increment( float value, float &number) {
+    std::scoped_lock<std::mutex> lock(mutex);
+    number += value;
+}
+void decrement(float value, float& number) {
+    std::scoped_lock<std::mutex> lock(mutex);
+    number -= value;
+}
+void multiply(float value, float& number) {
     std::scoped_lock<std::mutex> lock(mutex);
     number += value;
 }
 
-void decrement(float value) {
-    std::scoped_lock<std::mutex> lock(mutex);
-    number -= value;
-}
 
-void multiply(float value) {
-    std::scoped_lock<std::mutex> lock(mutex);
-    number *= value;
-}
 
 int main() {
-    std::jthread t1(increment, 2.3);
-    std::jthread t2(decrement, 1.5);
-    std::jthread t3(multiply, 3.0);
+    float number = 1.0;
+
+
+    auto t1 = std::jthread([&number]() { increment(2.3, number); });
+    auto t2 = std::jthread([&number]() { decrement(1.2, number); });
+    auto t3 = std::jthread([&number]() { multiply(3.6, number); });
+
+
 
     t1.join();
     t2.join();
