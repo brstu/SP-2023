6590b215e3372fa6211ddc8050a13663722f2c55
Anton Lobarev <loborew@gmail.com>
2023-11-06 22:00:25 +0300

    Add Lab_04

---
+++ trunk/PO210640/task_04/Lab_04/Lab_04/Shortcut.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+
+namespace Lab_04;
+
+public class Shortcut
+{
+    #region Fields
+
+    private int _nextId = 0;
+
+    private int _id;
+    private int _size;
+    private int _xPosition;
+    private int _yPosition;
+    private string _path;
+
+    private Action? _parentDoWork = null;
+
+    #endregion
+
+    public Shortcut(int size, int xPosition, int yPosition, string path)
+    {
+        _id = _nextId++;
+
+        _size = size;
+        _xPosition = xPosition;
+        _yPosition = yPosition;
+        _path = path;
+    }
+
+    public void SetParentDoWork(Action action)
+    {
+        _parentDoWork = action;
+    }
+
+    public Process Run()
+    {
+        var process = Process.Start(_path);
+
+        _parentDoWork?.Invoke();
+
+        return process;
+    }
+
+    public void Move(int x, int y)
+    {
+        _xPosition = x;
+        _yPosition = y;
+
+        _parentDoWork?.Invoke();
+    }
+
+    public bool IsShown(int height, int width)
+    {
+        return _yPosition >= 0 && _yPosition < height && _xPosition >= 0 && _xPosition < width;
+    }
+
+    public int GetSize()
+    {
+        return _size;
+    }
+}
---
+++ trunk/PO210640/task_04/Lab_04/Lab_04/Window.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab_04;
+
+public class Window
+{
+    #region Fields
+
+    private static int _nextId = 0;
+
+    protected int _id;
+    protected int _width;
+    protected int _height;
+    protected int _memoryNeeded;
+    protected bool _areAdministatorRightsGranted;
+    protected bool _isShown;
+
+    #endregion
+
+
+    public Window(int width, int height, bool areAdministatorRightsGranted)
+    {
+        _id = _nextId++;
+
+        _width = width;
+        _height = height;
+        _areAdministatorRightsGranted = areAdministatorRightsGranted;
+    }
+
+    protected virtual void DoWork()
+    {
+        // do smth
+    }
+
+    public void ShowOrHide(bool isShown)
+    {
+        _isShown = isShown;
+    }
+}
