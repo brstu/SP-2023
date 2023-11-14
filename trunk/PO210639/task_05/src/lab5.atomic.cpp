1c305c60af7c79e6e20881e2405fc0ab272fab42
ILIYA <99527242+KARMA-143@users.noreply.github.com>
2023-11-15 00:13:10 +0300

    [task_05] Add task_05

---
+++ trunk/PO210639/task_05/src/lab5.atomic.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void add(atomic<float>& result) {
+	result.store(result.load() + static_cast <float>(1.23123), std::memory_order_relaxed);
+}
+
+void sub(atomic<float>& result) {
+	result.store(result.load() - static_cast <float>(2.51234), std::memory_order_relaxed);
+}
+
+void mul(atomic<float>& result) {
+	result.store(result.load() * static_cast <float>(2.01235), std::memory_order_relaxed);
+}
+
+int main()
+{
+	atomic<float> result{ static_cast <float>(5.67293) };
+
+	std::jthread t1(add, std::ref(result));
+	std::jthread t2(sub, std::ref(result));
+	std::jthread t3(mul, std::ref(result));
+
+	t1.join();
+	t2.join();
+	t3.join();
+	cout << "result: " << result << endl;
+	return 0;
+}
