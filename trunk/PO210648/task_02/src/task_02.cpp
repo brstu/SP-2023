c37506dee20d9ae46e0d4186d5efb42fd237e188
HarWeasttt <Andry_1203@mail.ru>
2023-11-14 11:17:57 +0300

    fix issues

--- trunk/PO210648/task_02/src/task_02.cpp
+++ trunk/PO210648/task_02/src/task_02.cpp
@@ -17,13 +17,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
 	wc.lpszClassName = L"MainWindow";
 	RegisterClassEx(&wc);
 
 	// Создание главного окна
 	hWnd = CreateWindowEx(0, wc.lpszClassName, L"Лабораторная работа 2", WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, 300, 200, NULL, NULL, hInstance, NULL);
+		CW_USEDEFAULT, CW_USEDEFAULT, 300, 200, nullptr, nullptr, hInstance, nullptr);
 	if (!hWnd)
 	{
 		return -1;
@@ -31,18 +31,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	// Создание кнопки
 	hButton = CreateWindow(L"BUTTON", L"Увеличить", WS_VISIBLE | WS_CHILD,
-		200, 40, 70, 50, hWnd, (HMENU)2, hInstance, NULL);
+		200, 40, 70, 50, hWnd, (HMENU)2, hInstance, nullptr);
 	hButton = CreateWindow(L"BUTTON", L"Увеличить", WS_VISIBLE | WS_CHILD,
-		30, 40, 70, 50, hWnd, (HMENU)1, hInstance, NULL);
+		30, 40, 70, 50, hWnd, (HMENU)1, hInstance, nullptr);
 
 	// Создание метки для отображения числа
 	hLabel = CreateWindow(L"STATIC", L"0", WS_VISIBLE | WS_CHILD,
-		125, 40, 70, 50, hWnd, NULL, hInstance, NULL);
+		125, 40, 70, 50, hWnd, nullptr, hInstance, nullptr);
 
 	ShowWindow(hWnd, nCmdShow);
 
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
+	while (GetMessage(&msg, nullptr, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
