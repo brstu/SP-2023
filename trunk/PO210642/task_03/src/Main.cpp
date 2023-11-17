9814777ff81d7ab15c813b50fac334a291c6ad25
ValeriaPJul <ValeriaPJul@users.github.com>
2023-11-17 18:52:50 +0300

    Fix smell

--- trunk/PO210642/task_03/src/Main.cpp
+++ trunk/PO210642/task_03/src/Main.cpp
@@ -14,9 +14,7 @@ int main()
     std::cout << "Enter the coefficients a, b, and c: ";
     std::cin >> a >> b >> c;
 
-    int solutions = solveQuadraticEquation(a, b, c, x1, x2);
-
-    if (solutions == 0)
+    if (int solutions = solveQuadraticEquation(a, b, c, x1, x2); solutions == 0)
     {
         std::cout << "Complex roots: x1 = " << x1 << ", x2 = " << x2 << std::endl;
     }
