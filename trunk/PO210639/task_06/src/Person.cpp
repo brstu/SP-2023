6a21c3790e906b8bf10f1d67e695f85cc0f1fb10
Iliya Lipovik <99527242+KARMA-143@users.noreply.github.com>
2023-11-20 08:48:07 +0300

    [task_06] Add task_06 (#51)
    * [task_06] Add task_06
    
    * fix code smells
    
    * fix code smells
    
    * fix code smells and issues
    
    * fix code smells and issues
    
    * fix code smells and issues
    
    * fix code duplication and issues
    
    * fix code duplication and issues
    
    * fix code duplication and issues
    
    * fix code smells and issues
    
    * fix code smells
    
    * fix issue

---
+++ trunk/PO210639/task_06/src/Person.cpp
@@ -0,0 +1,29 @@
+﻿#include "Person.h"
+#include <iostream>
+#include <iomanip>
+#include <format>
+
+using namespace std;
+
+void Person::print(vector<string> fields)
+{
+	vector<string>::iterator it;
+	cout << "|";
+	it = std::ranges::find(fields.begin(), fields.end(), "id");
+	if (it != fields.end()) {
+		cout << std::format("{: ^10}", id)<< std::format("{: >10}", '|');
+	}
+	it = std::ranges::find(fields.begin(), fields.end(), "surname");
+	if (it != fields.end()) {
+		cout << std::format("{: ^17}", surname) << std::format("{: >10}", '|');
+	}
+	it = std::ranges::find(fields.begin(), fields.end(), "name");
+	if (it != fields.end()) {
+		cout << std::format("{: ^14}", name) << std::format("{: >10}", '|');
+	}
+	it = std::ranges::find(fields.begin(), fields.end(), "age");
+	if (it != fields.end()) {
+		cout << std::format("{: ^13}", age) << std::format("{: >10}", '|');
+	}
+	cout << endl;
+}
---
+++ trunk/PO210639/task_06/src/lab6.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include "Response.h"
+
+using namespace std;
+
+int main()
+{
+    system("color f0");
+    Response response;
+    string request;
+    string requestType="";
+    while (request != "end") {
+        getline(cin, request, '\n');
+        for (int i = 0; i < request.size() && request[i] != ' '; i++) {
+            requestType += request[i];
+        }
+        if (requestType == "SELECT") {
+            response.selectData(request);
+        }
+        else if (requestType == "INSERT") {
+            response.insertData(request);
+        }
+        else if (requestType == "DELETE") {
+            response.deleteData(request);
+        }
+        else if (requestType == "UPDATE") {
+            response.updateData(request);
+        }
+        else if (requestType == "end") {
+            return 0;
+        }
+        else {
+            cout << "Wrong request!" << endl;
+        }
+        request.clear();
+        requestType.clear();
+    }
+    
+    
+}
