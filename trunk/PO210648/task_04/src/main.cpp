3f37dd56cda019fde2b8e4f1a890aa4659744f61
Андрей Шлыков <99527318+HarWeasttt@users.noreply.github.com>
2023-12-15 22:01:11 +0300

    [task_04] add task_04 (#55)
    * [task_04] add task_04
    
    * [task_04] fix
    
    * [task_04] fix 2
    
    * [task_05] add task_05
    
    * Revert "[task_05] add task_05"
    
    This reverts commit 130eb2cb2a94845b3b2008dc506e8bf198cca5b1.
    
    * fix
    
    * fix
    
    * [task_04] fix
    
    * Update trunk/PO210648/task_04/src/CMakeLists.txt
    
    Co-authored-by: Yana Sitkovets  <yana.sitkowecz@yandex.ru>
    
    ---------
    
    Co-authored-by: Yana Sitkovets <yana.sitkowecz@yandex.ru>

---
+++ trunk/PO210648/task_04/src/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+
+class Window {
+private:
+    int id;
+    int height;
+    int width;
+    int memoryNeeded = 0;
+    bool areAdministatorRightsGranted;
+    bool isShown = false;
+public:
+    Window(int id, int height, int width, bool areAdministatorRightsGranted)
+        : id(id), height(height), width(width), memoryNeeded(), areAdministatorRightsGranted(areAdministatorRightsGranted), isShown() {}
+
+    virtual void doWork() {
+        memoryNeeded += 1;
+    }
+    void changeActiveScreen() const {
+        // Дополнительная логика изменения активного экрана
+        std::cout << "Changing active screen" << std::endl;
+    }
+
+    void showOrHide() {
+        isShown = !isShown;
+    }
+};
+
+
+class Shortcut {
+private:
+    int id;
+    int size;
+    int xPosition;
+    int yPosition;
+    std::string path;
+
+public:
+    Shortcut(int id, int size, int xPosition, int yPosition, const std::string& path)
+        : id(id), size(size), xPosition(xPosition), yPosition(yPosition), path(path) {}
+
+    void move(int newX, int newY) {
+        xPosition = newX;
+        yPosition = newY;
+    }
+
+    void launch() const {
+        std::cout << "Launching shortcut: " << path << std::endl;
+    }
+};
+
+class DesktopWindow : public Window {
+private:
+    std::vector<Shortcut> shortcuts;
+    std::string backgroundImagePath;
+
+public:
+    DesktopWindow(int id, int height, int width, bool areAdministatorRightsGranted, const std::string& backgroundImagePath)
+        : Window(id, height, width, areAdministatorRightsGranted), backgroundImagePath(backgroundImagePath) {}
+
+    void addShortcut(int shortcutId, int size, int xPosition, int yPosition, const std::string& path) {
+        Shortcut shortcut(shortcutId, size, xPosition, yPosition, path);
+        shortcuts.push_back(shortcut);
+        doWork();
+    }
+
+    
+};
+
+void createNewScreen(std::vector<Window>& windows) {
+    DesktopWindow newScreen(1, 1024, 768, true, "background.png");
+    newScreen.addShortcut(1, 50, 200, 200, "shortcut1.exe");
+    newScreen.addShortcut(2, 50, 300, 300, "shortcut2.exe");
+    windows.push_back(newScreen);
+}
+
+void changeActiveScreen(std::vector<Window>& windows, int index) {
+    if (index >= 0 && index < windows.size()) {
+       windows[index].changeActiveScreen();
+    } else {
+        std::cout << "Invalid screen index" << std::endl;
+    }
+}
+
+int main() {
+    std::vector<Window> windows;
+
+    createNewScreen(windows);
+
+    changeActiveScreen(windows, 0);
+
+    return 0;
+}
