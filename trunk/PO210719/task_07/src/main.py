5b5d8b2d2c1dcddbb5207ba0233d8f73b3abf417
Kodirov Behruz <99527212+KodirovBehruz@users.noreply.github.com>
2023-12-16 17:54:53 +0300

    Update main.py

--- trunk/PO210719/task_07/src/main.py
+++ trunk/PO210719/task_07/src/main.py
@@ -3,52 +3,62 @@ import time
 import queue
 import random
 
-orders_list = [{'salad': 3,
-                'pizza': 4,
-                'burger': 3,
-                'chicken': 5}]
 
-orders_queue = queue.Queue()
-flag = threading.Event()
-lock = threading.Lock()
+class Waiter(threading.Thread):
+    def __init__(self, name, orders_queue, orders_list, flag):
+        super().__init__()
+        self.name = name
+        self.orders_queue = orders_queue
+        self.orders_list = orders_list
+        self.flag = flag
 
-def waiters():
-    waiter_list = ['Официант1', 'Официант2', 'Официант3']
-    idx = 0
-    while not flag.is_set():
-        with lock:
-            if orders_list:
-                order = orders_list.pop(0)
-                for dish, cook_time in order.items():
-                    waiter = waiter_list[idx % len(waiter_list)]
-                    print(f'{waiter} принял заказ на {dish}')
-                    orders_queue.put((dish, cook_time))
-                    time.sleep(random.randint(1, 3))
-                    idx += 1
+    def run(self):
+        while not self.flag.is_set():
+            if self.orders_list:
+                order = self.orders_list.pop(0)
+                print(f'{self.name} принял заказ на {order}')
+                self.orders_queue.put(order)
+                time.sleep(random.randint(1, 3))
             else:
-                flag.set()
-                print('Официанты закончили принимать заказы')
+                self.flag.set()
+                print(f'{self.name} закончил принимать заказы')
 
-def cooker():
-    while not flag.is_set() or not orders_queue.empty():
-        if not orders_queue.empty():
-            dish, cook_time = orders_queue.get()
-            print(f'Повар начал готовить {dish}')
-            time.sleep(cook_time)
-            print(f'Повар закончил готовить {dish}')
-        else:
-            if not flag.is_set():
-                print('Повар ждет заказов')
-                break
-              
 
-waiter_thread = threading.Thread(target=waiters)
-cooker_thread = threading.Thread(target=cooker)
+class Chef(threading.Thread):
+    def __init__(self, name, orders_queue, flag):
+        super().__init__()
+        self.name = name
+        self.orders_queue = orders_queue
+        self.flag = flag
 
-waiter_thread.start()
-cooker_thread.start()
+    def run(self):
+        while not self.flag.is_set() or not self.orders_queue.empty():
+            if not self.orders_queue.empty():
+                order = self.orders_queue.get()
+                print(f'{self.name} начал готовить {order}')
+                time.sleep(random.randint(2, 5))
+                print(f'{self.name} закончил готовить {order}')
+            else:
+                if not self.flag.is_set():
+                    print(f'{self.name} ждет заказов')
 
-waiter_thread.join()
-cooker_thread.join()
 
-print("Ресторан закрыт")
+if __name__ == "__main__":
+    orders_queue = queue.Queue()
+    orders_list = ['Pizza', 'Burger', 'Salad'] 
+    flag = threading.Event()
+
+    waiter1 = Waiter('Официант 1', orders_queue, orders_list, flag)
+    waiter2 = Waiter('Официант 2', orders_queue, orders_list, flag)
+    waiter3 = Waiter('Официант 3', orders_queue, orders_list, flag)
+    chef = Chef('Повар', orders_queue, flag)
+
+    waiter1.start()
+    waiter2.start()
+    waiter3.start()
+    chef.start()
+
+    waiter1.join()
+    waiter2.join()
+    waiter3.join()
+    chef.join()
